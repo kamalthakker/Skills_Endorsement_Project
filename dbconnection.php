dd2bf978f3208c419c6b0213dee0fa332e5e08b1
Kamal Thakker <kamalthakker@Kamals-MacBook-Air.local>
2016-01-01 16:48:21 -0500

    dbconnection

---
+++ dbconnection.php
@@ -0,0 +1,19 @@
+<?php
+
+// Local DB
+
+$db_servername = "localhost";
+$db_username = "root";
+$db_password = "root";
+$db_name = "Skills_Endorsement";
+
+
+// MU DB
+/*
+ $db_servername = "localhost";
+ $db_username = "s0962803";
+ $db_password = "ahn8Thoh";
+ $db_name = "s0962803";
+*/ 
+
+?>
