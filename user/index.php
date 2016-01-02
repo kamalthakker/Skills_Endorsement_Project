ae2d5f0049083e3b71d9bdfa653c61d6687952e0
Kamal Thakker <kamalthakker@Kamals-MacBook-Air.local>
2016-01-02 15:41:21 -0500

    index

--- user/index.php
+++ user/index.php
@@ -1,4 +1,12 @@
-/*
-Log in
-*/
+<?php
+//session_start();
+$page_title = "Home";
+$linkno = 1;
+include_once 'includes/header.php';
+//include_once 'classes/user.php';
+?>
 
+
+<?php
+include_once 'includes/footer.php';
+?>
