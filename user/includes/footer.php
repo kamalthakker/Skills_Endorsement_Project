3b21ba295e8265a1b6c57d2aa1900e49b8ebee7e
Kamal Thakker <kamalthakker@Kamals-MacBook-Air.local>
2016-01-02 15:40:55 -0500

    footer

---
+++ user/includes/footer.php
@@ -0,0 +1,16 @@
+<?php
+// User's footer file
+?>
+
+<!-- begin #footer -->
+    <div id="footer">
+		<p>
+        	Terms of Use | <a title="This page validates as XHTML 1.0 Strict" href="http://validator.w3.org/check/referer" class="footerLink"><abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a> | 
+        <a title="This page validates as CSS" href="http://jigsaw.w3.org/css-validator/check/referer" class="footerLink"><abbr title="Cascading Style Sheets">CSS</abbr></a>  |  Copyright &copy; Team A. Design by <a href="#">Monmouth University</a>
+        </p>
+	</div>
+    <!-- end #footer -->
+</div>
+<!-- end #container -->
+</body>
+</html>
