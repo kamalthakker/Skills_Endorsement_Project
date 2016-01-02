04fbc6c6c9fa3e41ae726558ab8421b320ae176a
Kamal Thakker <kamalthakker@Kamals-MacBook-Air.local>
2016-01-02 17:07:43 -0500

    removed clutter

--- user/includes/footer.php
+++ user/includes/footer.php
@@ -5,8 +5,7 @@
 <!-- begin #footer -->
     <div id="footer">
 		<p>
-        	Terms of Use | <a title="This page validates as XHTML 1.0 Strict" href="http://validator.w3.org/check/referer" class="footerLink"><abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a> | 
-        <a title="This page validates as CSS" href="http://jigsaw.w3.org/css-validator/check/referer" class="footerLink"><abbr title="Cascading Style Sheets">CSS</abbr></a>  |  Copyright &copy; Team A. Design by <a href="#">Monmouth University</a>
+        	  Copyright &copy; Skills Endorsement. Design by <a href="#">Monmouth University</a>
         </p>
 	</div>
     <!-- end #footer -->
