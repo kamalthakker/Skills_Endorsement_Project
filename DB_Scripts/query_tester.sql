18fc4176cf8ac06bb8f452ecb40485e710ef325f
Kamal Thakker <kamalthakker@Kamals-MacBook-Air.local>
2016-01-19 18:58:07 -0500

    Recent activity + Endorsement suggestions queries

--- DB_Scripts/query_tester.sql
+++ DB_Scripts/query_tester.sql
@@ -29,17 +29,17 @@ VALUES
 select * from users	where user_id=1;
 
 /*All user's skills with endorsement rankings*/
-select userskills.user_id, userskills.skill_id, userskills.skill_name, skillrank.rank from 
+select distinct userskills.user_id, userskills.skill_id, userskills.skill_name, skillrank.rank from 
 (
 	select p.user_id, ps.skill_id, s.name as skill_name from project_skills ps 
 	inner join projects p on p.project_id=ps.project_id
 	inner join skills s on s.skill_id=ps.skill_id
- 	where upper(p.approved)='Y' and user_id=1
+ 	where upper(p.approved)='Y' and user_id=3
 ) userskills
 left join
 ( 
 	select skill_id, count(skill_id) as rank from  skill_endorsements
-	where user_id=1
+	where user_id=3
 	group by skill_id
 ) skillrank
 on userskills.skill_id=skillrank.skill_id
@@ -70,11 +70,68 @@ order by skillrank.rank desc, pskill.skill_name;
 
 /*Get user's endorsement*/
 select se.skill_endorsement_id, se.user_id, se.skill_id,  
-se.endorsed_by_user_id, se.endorsed_on, se.comments
+se.endorsed_by_user_id, se.endorsed_on, se.comments,
 u.fname, u.lname, u.job_title, u.userdp
 from skill_endorsements se
 inner join users u on se.endorsed_by_user_id=u.user_id
 where se.user_id=1 and se.skill_id=2918 
+order by se.endorsed_on desc, u.lname;
+
+
+/*Search results*/
+select distinct user_id, fname, lname, job_title, speciality, speciality2, userdp 
+from users
+where fname like '%mike%'
+
+
+union 
+
+select distinct u.user_id, fname, lname, job_title, speciality, speciality2, userdp 
+from project_skills ps 
+	inner join projects p on p.project_id=ps.project_id
+	inner join skills s on s.skill_id=ps.skill_id
+	inner join users u on u.user_id=p.user_id
+ 	where upper(p.approved)='Y' and (s.name like '%java%' or p.project_name like '%an%')
+order by fname;
+
+
+/*Search results*/ select distinct user_id, fname, lname, job_title, speciality, speciality2, userdp from users where fname like '%java%' union select distinct u.user_id, fname, lname, job_title, speciality, speciality2, userdp from project_skills ps inner join projects p on p.project_id=ps.project_id inner join skills s on s.skill_id=ps.skill_id inner join users u on u.user_id=p.user_id where upper(p.approved)='Y' and (s.name like '%java%' or p.project_name like '%java%') order by fname
+
+
+
+
+INSERT INTO skill_endorsements (skill_endorsement_id, user_id, skill_id, endorsed_by_user_id, comments)
+VALUES
+	(8, 1, 2918, 5, 'I write on behalf of xyz\'s application for a Rhodes Scholarship.  I believe abc to be an exemplary student-scholar as well as a person of tremendous character with significant leadership skills.\r\rI heard of xyz before I met her.  A top student of mine and Truman finalist came into my office and announced that she had met the next Truman finalist - a first year woman who was full of energy for leading the Students for Choice organization.  The young woman I encountered in my American Politics class that next semester was, in fact, quite enthusiastic for her studies and a solid, conscientious student.  Full of questions, xyz regularly participated in class discussions and probed the materials with a zest for learning.  In her first year, however, I was more struck by her openness to the world than her analytic ability.   ', '2016-01-16 16:39:30');
+
+
+
+/*Endorsement suggestions*/
+select p2.project_id, p2.project_name, u.user_id, u.fname, u.lname, 
+case when already_endorsed.cskill is null then 
+FLOOR(RAND() * 15) + 1 else already_endorsed.cskill end as cskill
+ from projects p1
+inner join projects p2 on
+p1.project_name=p2.project_name and p2.user_id<>1 and p2.approved='Y'
+inner join users u on
+p2.user_id=u.user_id
+left join 
+(select user_id, count(skill_id)*100 cskill from skill_endorsements
+where endorsed_by_user_id=1) already_endorsed on
+already_endorsed.user_id=u.user_id
+where p1.approved='Y' and p1.user_id=1 
+order by cskill
+limit 5;
+
+
+/*Recent activity*/
+select se.*, s.name from skill_endorsements se
+inner join skills s on
+se.skill_id=s.skill_id
+order by se.endorsed_on desc
+limit 5;
+
+
 
 
  
