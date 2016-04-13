981df3f0ca1f37048d6b814fefdb57e9aa8ac067
kamalthakker <kamalthakker@gmail.com>
2016-04-13 15:33:46 -0400

    delete project

---
+++ user/delete_project.php
@@ -0,0 +1,2 @@
+<?php include_once 'classes/project.php'; if(isset($_REQUEST['project_id'])) {$project_id=$_REQUEST['project_id'];$objProject = new project();/*$result = $objProject->deleteProject($project_id);*/header( "Location: manage_projects.php" );}
+?>
